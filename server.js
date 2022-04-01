8721ec47a7667711f364bc6c044a4bd7cc4a9609
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-04-01 17:50:05 -0500

    adding format partials

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const session = require("express-session");
+const helpers = require("./utils/helpers");
 
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
@@ -23,7 +24,7 @@ const sess = {
 app.use(session(sess));
 
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
